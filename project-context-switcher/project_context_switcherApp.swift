adc954baca3a4f7ead77f69d98347d604bfc38dc
matt-pasek <m_pasek@icloud.com>
2025-11-26 01:22:25 +0200

    Initial Commit

---
+++ project-context-switcher/project_context_switcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_context_switcherApp.swift
+//  project-context-switcher
+//
+//  Created by Mateusz Pasek on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct project_context_switcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
