8d4216f9a61d4557df42ca2ae8df66cf70c0567a
matt-pasek <m_pasek@icloud.com>
2025-11-27 17:51:53 +0200

    ProjectManager init definition

---
+++ project-context-switcher/Services/ProjectManager.swift
@@ -0,0 +1,41 @@
+//
+//  ProjectManager.swift
+//  project-context-switcher
+//
+//  Created by Mateusz Pasek on 27/11/2025.
+//
+
+import Foundation
+
+@Observable class ProjectManager {
+    var currentRunning: Project?
+    var projects: [Project] = []
+    
+    func addProject(_ project: Project) {
+        projects.append(project)
+    }
+    
+    func removeProject(_ project: Project) {
+        projects.removeAll(where: { $0.id == project.id })
+    }
+    
+    func switchTo(_ project: Project) {
+        currentRunning = project
+        // TODO: implement full switching logic
+    }
+    
+    func updateProjectStatus(_ project: Project, status: ProjectStatus) {
+        project.status = status
+    }
+    
+    func stopCurrentProject() {
+        guard let current = currentRunning else { return }
+        
+        // lets pretend this is stopping logic
+        updateProjectStatus(currentRunning!, status: .stopping)
+        // TODO: actually stop the project
+        updateProjectStatus(currentRunning!, status: .idle)
+
+        currentRunning = nil
+    }
+}
